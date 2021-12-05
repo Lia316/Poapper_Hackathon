c5056ff78741d3c77ef697605df33ef06c9d428e
Lia <kjk0825@postech.ac.kr>
2021-12-06 02:45:41 +0900

    fix: 🐞 heroku db error - set port

--- BE/complaint.js
+++ BE/complaint.js
@@ -7,6 +7,7 @@ require('dotenv').config()
 const db = mysql.createConnection({
     host: process.env.HOST,
     user: process.env.USER,
+    port: process.env.PORT,
     password: process.env.PASSWORD,
     database: process.env.DATABASE
 })
